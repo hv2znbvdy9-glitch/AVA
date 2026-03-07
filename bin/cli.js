8ffc1917104b20bd3f1b55d879ea83b6597da22b
hv2znbvdy9-glitch <hv2znbvdy9@privaterelay.appleid.com>
2026-03-07 18:55:30 +0100

    Revert "Add #0969DA brand color to AVA CLI output (#21)"
    This reverts commit 7447dfebc33ac0ef27d95ecd26611408025fa035.

--- bin/cli.js
+++ bin/cli.js
@@ -2,13 +2,12 @@
 'use strict';
 
 const {run} = require('../src/run');
-const {ava} = require('../src/color');
 
 const args = process.argv.slice(2);
 
 if (args.length === 0) {
-	console.error(`Usage: ${ava('ava')} <command>`);
-	console.error(`Example: ${ava('ava')} "echo hello"`);
+	console.error('Usage: ava <command>');
+	console.error('Example: ava "echo hello"');
 	process.exit(1);
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 'use strict';
 
 const {run, runAll} = require('./run');
-const {hexToRgb, colorize, ava, AVA_COLOR} = require('./color');
 
-module.exports = {run, runAll, hexToRgb, colorize, ava, AVA_COLOR};
+module.exports = {run, runAll};
