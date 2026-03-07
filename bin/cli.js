d8617b9cc1a9218bd89c8c06a5775b12e58121a0
hv2znbvdy9-glitch <hv2znbvdy9@privaterelay.appleid.com>
2026-03-07 18:39:47 +0100

    Revert "Add brand color, session overview, and CLI flags to AVA (#24)" (#28)
    This reverts commit 10b38d096fad66028bcf8b95922a7228dce04aeb.

--- bin/cli.js
+++ bin/cli.js
@@ -2,20 +2,8 @@
 'use strict';
 
 const {run} = require('../src/run');
-const {ava: brandColor, AVA_COLOR} = require('../src/color');
-const {overview} = require('../src/overview');
 
 const args = process.argv.slice(2);
-
-if (args[0] === '--color') {
-	console.log(`AVA brand color: ${brandColor()} (${AVA_COLOR})`);
-	process.exit(0);
-}
-
-if (args[0] === '--overview') {
-	console.log(overview);
-	process.exit(0);
-}
 
 if (args.length === 0) {
 	console.error('Usage: ava <command>');
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 'use strict';
 
 const {run} = require('./run');
-const {hexToRgb, colorize, ava, AVA_COLOR} = require('./color');
-const {overview} = require('./overview');
 
-module.exports = {run, hexToRgb, colorize, ava, AVA_COLOR, overview};
+module.exports = {run};
